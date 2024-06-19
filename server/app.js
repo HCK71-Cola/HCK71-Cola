b9c68b93f42d73821ed6c3d6f8b2b84002f8bb64
zainalalmasyhur <zainalalmasyhur30@gmail.com>
2024-06-20 00:10:46 +0700

    fix: fix controller name

--- server/app.js
+++ server/app.js
@@ -1,50 +1,47 @@
-const express = require('express');
-const { createServer } = require('http');
+const express = require("express");
+const { createServer } = require("http");
 // const { join } = require('node:path');
-const { Server } = require('socket.io');
-const {Message} = require("./models/index")
+const { Server } = require("socket.io");
+const { Message } = require("./models/index");
 const cors = require("cors");
-const { userController } = require('./controllers/userController');
+const { userCtrl } = require("./controllers/userCtrl");
 
 const app = express();
 const server = createServer(app);
 
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(cors())
+app.use(cors());
 
+app.post("/register", userCtrl.register);
+app.post("/login", userCtrl.login);
 
-app.post("/register", userController.register)
-app.post("/login", userController.login)
-
-
-const io = new Server(server,{
-    cors: {
-        origin: "*"
-    }
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
 });
 
 io.on("error", (error) => {
-    console.log(error);
-})
-
-io.on('connection', (socket) => {
-    console.log('a user connected njay');
-
-    socket.on("messages", async ()=> {
-
-        const messages = await Message.findAll()
-        io.emit("messages",messages )
-    }) 
-
-    socket.on("messages:post", async (body) => {
-        console.log(body);
-        await Message.create(body)
-
-        const messages = await Message.findAll()
-        io.emit("messages",messages ) 
-    })
+  console.log(error);
 });
 
-server.listen(3000, () => console.log(3000))
+io.on("connection", (socket) => {
+  console.log("a user connected njay");
+
+  socket.on("messages", async () => {
+    const messages = await Message.findAll();
+    io.emit("messages", messages);
+  });
+
+  socket.on("messages:post", async (body) => {
+    console.log(body);
+    await Message.create(body);
+
+    const messages = await Message.findAll();
+    io.emit("messages", messages);
+  });
+});
+
+server.listen(3000, () => console.log(3000));
